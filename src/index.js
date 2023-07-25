92ee58bc3267c8648c0fcdb2f5081f609da8ea8f
ttulov3 <87874297+ttulov3@users.noreply.github.com>
2023-07-25 14:14:08 +0000

    isa

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const crypto = require("crypto")
 const express = require("express")
 const fs = require("fs")
 const { getDatabaseInstance } = require("./database")
@@ -7,26 +8,64 @@ const app = express()
 app.use(express.static(__dirname + '/../public'))
 app.use(express.json())
 
-app.post("/movies", async (req, res) => {
+const loginTokens = []
+
+// VERIFICAR LOGIN
+function login(req, res, next) {
+  const { token } = req.query
+  if (loginTokens.includes(token)) {
+    next()
+    return
+  }
+  res.status(400).json({ error: true, msg: "token de acesso inválido!" })
+}
+
+// LOGIN
+app.get("/login", (req, res) => {
+  const { login, senha } = req.query
+  if (login == "daniel" && senha == "123123") {
+    const hash = crypto.randomBytes(20).toString('hex')
+    loginTokens.push(hash)
+    console.log(hash)
+    res.json({ error: false, token: hash })
+    return
+  }
+  res.status(400).json({ error: true, msg: "usuário e senha inválidos" })
+})
+
+
+// CREATE
+app.post("/movies", login, async (req, res) => {
   const { title, source, description, thumb } = req.body
   const db = await getDatabaseInstance()
   const result = await db.run(`INSERT INTO movies(title, source, description, thumb) VALUES(?, ?, ?, ?)`, [title, source, description, thumb])
   res.json(result)
 })
 
-app.get("/movies", async (req, res) => {
+// READ
+app.get("/movies", login, async (req, res) => {
   const { id } = req.query
   const db = await getDatabaseInstance()
   if (id) {
-    const result = await db.get(`SELECT * FROM movies WHERE id=?`, id)
+    const result = await db.all(`SELECT * FROM movies WHERE id=?`, id)
     res.json(result)
     return
   }
-  const result = await db.all(`SELECT * FROM movies`)
+  const result = await db.all(`SELECT * FROM movies ORDER BY id DESC`)
   res.json(result)
 })
 
-app.put("/movies", async (req, res) => {
+// READ
+app.get("/movies/:id", login, async (req, res) => {
+  const { id } = req.params
+  const db = await getDatabaseInstance()
+  const result = await db.get(`SELECT * FROM movies WHERE id=?`, id)
+  res.json(result)
+  return
+})
+
+// UPDATE
+app.put("/movies", login, async (req, res) => {
   const { id } = req.query
   const { title, source, description, thumb } = req.body
   const db = await getDatabaseInstance()
@@ -37,19 +76,22 @@ app.put("/movies", async (req, res) => {
   res.json(result)
 })
 
-app.patch("/movies", async (req, res) => {
+// UPDATE
+app.patch("/movies", login, async (req, res) => {
   const { id } = req.query
   const db = await getDatabaseInstance()
-  const sets = Object.keys(req.body).map(key => `${key}=?`).join(", ")
-  const values = Object.values(req.body)
+  // const sets = Object.keys(req.body).join("=?, ") + "=?"
+  const sets = Object.keys(req.body).map(k => `${k}=?`).join(", ")
+  const values = [...Object.values(req.body), id]
   const result = await db.run(`UPDATE movies SET ${sets} WHERE id=?`, values)
   res.json(result)
 })
 
-app.delete("/movies", async (req, res) => {
+// DELETE
+app.delete("/movies", login, async (req, res) => {
   const { id } = req.query
   const db = await getDatabaseInstance()
-  const result = await db.run(`DELETE FROM movies WHERE id=?`, [id])
+  const result = await db.run(`DELETE FROM movies WHERE id=?`, id)
   res.json(result)
 })
 
